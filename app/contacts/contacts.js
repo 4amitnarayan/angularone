78407e1343133491ddb0153520b59851448b1777
PRIYANKA KUMAR <priyankakumar@PRIYANKAs-MacBook-Pro.local>
2017-07-01 16:04:32 +0800

    Added Local Data

--- app/contacts/contacts.js
+++ app/contacts/contacts.js
@@ -11,7 +11,6 @@ angular.module('myContacts.contacts', ['ngRoute'])
 
 //Contact Controller
 .controller('ContactsCtrl', ['$scope',  function($scope) {
-	//Init Firebase
   var user_Id = 1;
 	$scope.contacts =  [
         {
@@ -19,235 +18,145 @@ angular.module('myContacts.contacts', ['ngRoute'])
             'name': 'Mark Green',
             'company': 'JP Morgan',
             'email':'mark@jpm.com',
-            'phones': [{
-              'mobile': '000-234-23423',
-              'home': '2312-32432-324',
-              'work': '000-234234-234'
-          }],
-          'address': [{
+            'mobile_phone': '000-234-23423',
+            'home_phone': '2312-32432-324',
+            'work_phone': '000-234234-234',
             'street_address': '10 Downing Streeet',
             'city': 'New York',
             'state': 'New York',
             'zipcode': '10001'
-          }]
         },{
             'id':1,
             'name': 'James Nash',
             'company': 'Morgan Stanley',
             'email':'james@ms.com',
-            'phones': [{
-              'mobile': '000-234-23423',
-              'home': '2312-32432-324',
-              'work': '000-234234-234'
-          }],
-          'address': [{
+            'mobile_phone': '000-234-23423',
+            'home_phone': '2312-32432-324',
+            'work_phone': '000-234234-234',
             'street_address': '10 Downing Streeet',
             'city': 'New York',
             'state': 'New York',
             'zipcode': '10001'
-          }]
         },{
             'id':2,
             'name': 'Paul King',
             'company': 'Deloitte',
             'email':'paul@deloitte.com',
-            'phones': [{
-              'mobile': '000-234-23423',
-              'home': '2312-32432-324',
-              'work': '000-234234-234'
-          }],
-          'address': [{
+            'mobile_phone': '000-234-23423',
+            'home_phone': '2312-32432-324',
+            'work_phone': '000-234234-234',
             'street_address': '10 Downing Streeet',
             'city': 'New York',
             'state': 'New York',
             'zipcode': '10001'
-          }]
         },{
             'id':3,
             'name': 'Stephen Greg',
             'company': 'Bank of America',
             'email':'stephen@ba.com',
-            'phones': [{
-              'mobile': '000-234-23423',
-              'home': '2312-32432-324',
-              'work': '000-234234-234'
-          }],
-          'address': [{
+            'mobile_phone': '000-234-23423',
+            'home_phone': '2312-32432-324',
+            'work_phone': '000-234234-234',
             'street_address': '10 Downing Streeet',
             'city': 'New York',
             'state': 'New York',
             'zipcode': '10001'
-          }]
         },{
             'id':4,
             'name': 'Monica Adam',
             'company': 'City Bank',
             'email':'monica@citi.com',
-            'phones': [{
-              'mobile': '000-234-23423',
-              'home': '2312-32432-324',
-              'work': '000-234234-234'
-          }],
-          'address': [{
+            'mobile_phone': '000-234-23423',
+            'home_phone': '2312-32432-324',
+            'work_phone': '000-234234-234',
             'street_address': '10 Downing Streeet',
             'city': 'New York',
             'state': 'New York',
             'zipcode': '10001'
-          }]
         },
     ];
 	console.log($scope.contacts);
 
-	//Show Add Form
-	$scope.showAddForm = function(){
-		$scope.addFormShow = true;
-	}
+	//Sumbit Contacts
+  $scope.addFormSubmit = function(){
+		console.log('Adding Contact....');
+    var i = 0;
+        if($scope.newcontact.id == null) {
+             $scope.newcontact.id = user_Id++;
+             $scope.contacts.push($scope.newcontact);
+        } else {
+             for(i in $scope.contacts) {
+                    if($scope.contacts[i].id == $scope.newcontact.id) {
+                        $scope.contacts[i] = $scope.newcontact;
+                    }
+             }
+        }
+        $scope.newcontact = {};
+        console.log($scope.newcontact);
+        console.log($scope.contacts);
+    }
+
+    //Delete Contact
+    $scope.removeContact = function(id) {
+      $scope.contactShow = false;
+      var i = 0;
+      for(i in $scope.contacts) {
+          if($scope.contacts[i].id == id) {
+              $scope.contacts.splice(i,1);
+              $scope.newcontact = {};
+          }
+      }
+  		console.log('Removing Contact...');
+  		$scope.msg = "Contact Removed";
+    }
 
 	//Show Edit Form
-	$scope.showEditForm = function(contact){
-		$scope.editFormShow = true;
-
-		$scope.id = contact.$id;
-		$scope.name = contact.name;
-		$scope.email = contact.email;
-		$scope.company = contact.company;
-		$scope.mobile_phone = contact.phones[0].mobile;
-		$scope.home_phone = contact.phones[0].home;
-		$scope.work_phone = contact.phones[0].work;
-		$scope.street_address = contact.address[0].street_address;
-		$scope.city = contact.address[0].city;
-		$scope.state = contact.address[0].state;
-		$scope.zipcode = contact.address[0].zipcode;
-	}
-
-	//Hide Forms
-	$scope.hide = function(){
-		$scope.addFormShow = false;
-		$scope.contactShow = false;
-	}
-
-	//Sumbit Contacts
-	$scope.addFormSubmit = function(){
-		console.log('Adding Contact....');
-
-		//Assign Values
-		if($scope.name){ var name = $scope.name; } else { var name = null; }
-		if($scope.email){ var email = $scope.email; } else { var email = null; }
-		if($scope.company){ var company = $scope.company; } else { var company = null; }
-		if($scope.mobile_phone){ var mobile_phone = $scope.mobile_phone; } else { var mobile_phone = null; }
-		if($scope.home_phone){ var home_phone = $scope.home_phone; } else { var home_phone = null; }
-		if($scope.work_phone){ var work_phone = $scope.work_phone; } else { var work_phone = null; }
-		if($scope.street_address){ var street_address = $scope.street_address; } else { var street_address = null; }
-		if($scope.city){ var city = $scope.city; } else { var city = null; }
-		if($scope.state){ var state = $scope.state; } else { var state = null; }
-		if($scope.zipcode){ var zipcode = $scope.zipcode; } else { var zipcode = null; }
-
-		//Build object
-		$scope.contacts.$add({
-			name: name,
-			email: email,
-			company: company,
-			phones: [{
-				mobile: mobile_phone,
-				home: home_phone,
-				work: work_phone
-			}],
-			address: [{
-				street_address: street_address,
-				city: city,
-				state: state,
-				zipcode: zipcode
-			}]
-		}).then(function(ref){
-			var id = ref.key();
-			console.log('Added contact with ID: ' + id);
-
-			//Clear Form
-			clearFields();
-
-			//hide Form
-			$scope.addFormShow = false;
-
-			//Send Message
-			$scope.msg = "Contact Added";
-		})
-	}
-
-	//Update Contacts
-	$scope.editFormSubmit = function(){
-		console.log('Updating Contact....');
-
-		// Get ID
-		var id = $scope.id;
-
-		//Get Record
-		var record = $scope.contacts.$getRecord(id);
-
-		//Assign Values
-		record.name = $scope.name;
-		record.email = $scope.email;
-		record.company = $scope.company;
-		record.phones[0].mobile = 	$scope.mobile_phone;
-		record.phones[0].home = $scope.home_phone;
-		record.phones[0].work = $scope.work_phone;
-		record.address[0].street_address = $scope.street_address;
-		record.address[0].city = $scope.city;
-		record.address[0].state = $scope.state;
-		record.address[0].zipcode = $scope.zipcode;
-
-		// Save Contact
-		$scope.contacts.$save(record).then(function(ref){
-			console.log(ref.key);
-		});
-
-		clearFields();
-
-		//hide Form
-		$scope.editFormShow = false;
-
-		//Send Message
-		$scope.msg = "Contact Updated";
-	}
-
-	//Clear $scope Fields
-	function clearFields(){
-		console.log('Clearing All Fields...');
-
-		$scope.name = '';
-		$scope.email = '';
-		$scope.company = '';
-		$scope.mobile_phone = '';
-		$scope.home_phone = '';
-		$scope.work_phone = '';
-		$scope.street_address = '';
-		$scope.city = '';
-		$scope.state = '';
-		$scope.zipcode = '';
+	$scope.showEditForm = function(id){
+		$scope.addFormShow = true;
+    console.log($scope.contacts);
+    var i = 0;
+    for(i in $scope.contacts) {
+        if($scope.contacts[i].id == id) {
+            $scope.newcontact = angular.copy($scope.contacts[i]);
+        }
+    }
 	}
 
 	//Show Contact
-	$scope.showContact = function(contact){
+	$scope.showContact = function(id){
 		console.log('Getting Contact...');
-
-		$scope.name = contact.name;
-		$scope.email = contact.email;
-		$scope.company = contact.company;
-		$scope.mobile_phone = contact.phones[0].mobile;
-		$scope.home_phone = contact.phones[0].home;
-		$scope.work_phone = contact.phones[0].work;
-		$scope.street_address = contact.address[0].street_address;
-		$scope.city = contact.address[0].city;
-		$scope.state = contact.address[0].state;
-		$scope.zipcode = contact.address[0].zipcode;
-
+    var i = 0;
+    for(i in $scope.contacts) {
+        if($scope.contacts[i].id == id) {
+            $scope.newcontact = angular.copy($scope.contacts[i]);
+        }
+    }
 		$scope.contactShow = true;
 	}
 
-	//Delete Contact
-	$scope.removeContact = function(contact) {
-		console.log('Removing Contact...');
-		$scope.contacts.$remove(contact);
-		$scope.msg = "Contact Removed";
-	}
+//Clear $scope Fields
+function clearFields(){
+  console.log('Clearing All Fields...');
+  $scope.name = '';
+  $scope.email = '';
+  $scope.company = '';
+  $scope.mobile_phone = '';
+  $scope.home_phone = '';
+  $scope.work_phone = '';
+  $scope.street_address = '';
+  $scope.city = '';
+  $scope.state = '';
+  $scope.zipcode = '';
+}
 
+//Show Add Form
+$scope.showAddForm = function(){
+  $scope.addFormShow = true;
+}
+
+//Hide Forms
+$scope.hide = function(){
+  $scope.addFormShow = false;
+  $scope.contactShow = false;
+}
 }]);
